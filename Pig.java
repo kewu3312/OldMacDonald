d468055cade72f9e92e6b2761676fb23c0af261c
kewu2 <kewu2@s.sfusd.edu>
2017-11-13 14:15:08 -0800

    third try

--- Pig.java
+++ Pig.java
@@ -1,4 +1,14 @@
-class Pig //implements Animal 
-{     
-     //your code here
+class Pig implements Animal {     
+     private String myType;     
+     private String quack;      
+     public Pig(String type, String sound)     {         
+         myType = type;         
+         quack = sound;     
+     }     
+     public Pig()     {         
+         myType = "unknown";         
+         quack = "unknown";     
+     }      
+     public String getSound(){return quack;}     
+     public String getType(){return myType;} 
 }
